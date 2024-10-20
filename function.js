fb1daf0c1f64a4fd2db98c7e4d25740498dac369
sumantacharya <sumanthacharya399@gmail.com>
2024-10-20 15:12:07 +0530

    javaScript function

---
+++ function.js
@@ -0,0 +1,23 @@
+// *****function*****
+
+
+/*function myFunc() {
+    let a = 20;
+    let b = 30;
+    let c = a + b;
+    console.log(c);
+}
+myFunc();
+// **********Arrow line fuction******
+myFunction = (s, g) => {
+    a = g;
+    b = s;
+    c = a - b;
+    alert("succesfull");
+    return c;
+}
+// myFunc();
+console.log(myFunction(70, 80));*/
+
+
+

78c5130c8847091238d1c938a8274f28d3e1b078
sumantacharya <sumanthacharya399@gmail.com>
2024-10-20 15:06:53 +0530

    JavaScript Array Example

---
+++ javascript.js
@@ -0,0 +1,40 @@
+console.log("hi sumanth");
+
+
+// Array************
+
+let myArray = ["sumanth", "star"];
+console.log(myArray[0], myArray[1]);
+
+let value = ["a", "b", "c", "d"];
+value[0] = "A";
+console.log(value);
+value[1] = "B";
+value[2] = "C";
+value[3] = "D";
+value.push("E");
+console.log(value);
+value.pop();
+console.log(value);
+value.unshift("sumanth");
+console.log(value);
+value.shift();
+console.log(value);
+value.splice(4, 0, 'e');
+console.log(value);
+value.splice(5, 0, 'sumanth', 'acharya');
+console.log(value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
